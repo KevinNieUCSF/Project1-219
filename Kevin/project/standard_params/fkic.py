6ec31e55e775bba963ba12aad3cfb0aa3c659c60
Chuankai Nie <kevinnie@dev1.wynton.ucsf.edu>
2021-04-10 13:18:57 -0700

    roseasy workspace

---
+++ Kevin/project/standard_params/fkic.py
@@ -0,0 +1,71 @@
+from pyrosetta import init
+from pyrosetta import pose_from_file
+from pyrosetta.rosetta.core.scoring import CA_rmsd
+from pyrosetta.rosetta.core.scoring import all_atom_rmsd
+from pyrosetta.rosetta.core.pose import setPoseExtraScore
+from roseasy import pipeline
+from roseasy import big_jobs
+#from roseasy.standard_params.filters import FilterContainer
+import os, sys, subprocess
+
+
+def get_workspace(root_dir, step):
+    '''
+    Provides information to the submission script about the workspace
+    '''
+    return pipeline.ValidationWorkspace(root_dir, step)
+
+
+if __name__=="__main__":
+    from roseasy.movers.loopmodeler import LoopModeler
+    '''Get information about the task to be run'''
+    workspace, job_info = big_jobs.initiate()
+    test_run = job_info.get('test_run', False)
+    pdbpath = workspace.input_path(job_info)
+
+    init()
+    pose = pose_from_file(pdbpath)
+    # RosEasy loopmodeler object
+    lm = LoopModeler()
+    # Fragment flags taken care of by workspace
+    lm.fragments_flags = workspace.fragments_flags(pdbpath)
+    # Config loop modeler for fkic
+    lm.config = 'fkic'
+    # Get loops from loops file; can also get loops from a range of
+    # residues
+    lm.loops_from_file(workspace.loops_path)
+    # Argument needed for FragmentScoreFilter & BUNS
+    dalphaball_path = os.path.join(workspace.rosetta_dir, 'source',
+            'external', 'DAlpahBall', 'DAlphaBall.gcc')
+    lm.add_init_arg('-holes:dalphaball {} -in:file:s {}'.format(dalphaball_path, pdbpath))
+    lm.add_init_arg('-in:file:s {}'.format(pdbpath))
+    if test_run:
+        # What happens when --test-run is supplied?
+        lm.mark_as_test_run()
+
+    # Add pose and apply
+    lm.pose = pose
+    lm.apply()
+
+    # Get and run default filters
+    filters = workspace.get_filters(lm.pose,
+            task_id=job_info['task_id'], score_fragments=True,
+            test_run=test_run)
+    filters.run_filters()
+
+    # Calculate RMSD from input
+    input_pose = pose_from_file(workspace.input_path(job_info))
+    ca_rmsd = CA_rmsd(lm.pose, input_pose)
+    all_atom_rmsd = all_atom_rmsd(lm.pose, input_pose)
+    loop = workspace.largest_loop
+    rmsd = CA_rmsd(input_pose, lm.pose, loop.start,
+            loop.end)
+
+    # Add RMSDs to pose for parsing
+    setPoseExtraScore(lm.pose, 'EXTRA_METRIC_CA_RMSD', ca_rmsd)
+    setPoseExtraScore(lm.pose, 'EXTRA_METRIC_AllAtom_RMSD', all_atom_rmsd)
+    setPoseExtraScore(lm.pose, 'EXTRA_METRIC_Loop_RMSD', rmsd)
+
+    if not os.path.exists(workspace.output_prefix(job_info)):
+        os.mkdir(workspace.output_prefix(job_info))
+    pose.dump_pdb(workspace.output_path(job_info))
---
+++ Kevin/project/standard_params/relax_with_cst.py
@@ -0,0 +1,52 @@
+from pyrosetta import init
+from pyrosetta.rosetta.core.pose import setPoseExtraScore
+from pyrosetta import pose_from_file
+from pyrosetta.rosetta.core.scoring import CA_rmsd
+from pyrosetta.rosetta.core.scoring import all_atom_rmsd
+from roseasy import pipeline
+from roseasy import big_jobs
+import os, sys, subprocess, gzip
+from roseasy.movers import relax as r
+#from roseasy.standard_params.filters import FilterContainer
+
+def get_workspace(root_dir, step):
+    return pipeline.DesignWorkspace(root_dir, step)
+
+if __name__=='__main__':
+    workspace, job_info = big_jobs.initiate()
+    test_run = job_info.get('test_run', False)
+    init('-relax:constrain_relax_to_start_coords -total_threads 1')
+    pdbpath = workspace.input_path(job_info)
+    pose = pose_from_file(workspace.input_pdb_path)
+    relax = r.Relax()
+
+    dalphaball_path = os.path.join(workspace.rosetta_dir, 'source',
+            'external', 'DAlpahBall', 'DAlphaBall.gcc')
+    relax.add_init_arg('-holes:dalphaball {} -in:file:s {}'.format(dalphaball_path, pdbpath))
+    relax.add_init_arg('-total_threads 1')
+    if test_run:
+        relax.rounds = 1
+    relax.pose = pose
+    # Warning: This is an all-atom movemap. Constrain to input coords if
+    # you don't want things to move around a lot, or set a different
+    # movemap.
+    relax.setup_default_movemap()
+    # Constrain relax to start coords.
+    relax.mover.constrain_relax_to_start_coords(True)
+    relax.apply()
+
+    input_pose = pose_from_file(workspace.input_pdb_path)
+    ca_rmsd = CA_rmsd(relax.pose, input_pose)
+    score_fragments = os.path.exists(workspace.loops_path)
+
+    filters = workspace.get_filters(relax.pose, 
+            task_id=job_info['task_id'], score_fragments=score_fragments,
+            test_run=test_run)
+    filters.run_filters()
+
+    input_name = os.path.basename(pdbpath).split(".")[0]
+    out = workspace.output_prefix(job_info) + input_name + workspace.output_suffix(job_info) + '.pdb.gz'
+
+    setPoseExtraScore(relax.pose, 'EXTRA_METRIC_CA_RMSD', ca_rmsd)
+
+    pose.dump_pdb(out)
